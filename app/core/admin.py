44c5e2e54798f266d9d25de471e1dc7fba90ed02
mahima <mahima@test.com>
2024-10-01 03:12:45 +0530

    UsewrToken_diff

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_(' Personal Info ', {'fields': ('name',)})),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -39,7 +39,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
